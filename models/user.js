3a40e6cab552bbcc587766e988ecc67c27ef508c
yodji09 <yodji09@gmail.com>
2020-04-23 13:09:00 +0700

    testing

--- models/user.js
+++ models/user.js
@@ -2,11 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    birthdate: DataTypes.DATE,
-    email: DataTypes.STRING,
+    Last_name: DataTypes.STRING,
+    Birthdate: DataTypes.DATE,
+    password : DataTypes.STRING,
+    Email: DataTypes.STRING,
     phone_number: DataTypes.STRING,
-    money: DataTypes.INTEGER
+    money: DataTypes.INTEGER,
   }, {});
   User.associate = function(models) {
     // associations can be defined here
